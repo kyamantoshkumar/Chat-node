bf491cf9592369f65d6268fa9f81b8afc057fae5
Mantosh <kyamantoshkumar@gmail.com>
2023-01-01 18:29:53 +0530

    Create coonection.js

---
+++ src/Components/connection/coonection.js
@@ -0,0 +1,25 @@
+const  mongoose = require ("mongoose");
+ const connectDatabase = () => {
+    mongoose.set('strictQuery', true);
+  mongoose
+    .connect("mongodb+srv://Mantosh:hellomongo@cluster0.vqogskc.mongodb.net/?retryWrites=true&w=majority")
+    .then((con) => {
+      console.log(`MongoDb is Connected to ${con.connection.host}`);
+    })
+    .catch((e) => {
+      console.log(`Error occured : ${e}`);
+    });
+};
+// const connectDatabase=()=>{
+//     mongoose.set('strictQuery', true)
+//     mongoose.connect("mongodb://localhost:27017/student", {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true,
+//     }).then(() => {
+//         console.log(`connection successfully`);
+//     }).catch((e) => {
+//         console.log(`no connection`, e);
+//     })
+//     
+// }
+module.exports = connectDatabase
