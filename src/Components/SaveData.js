a02118ae3d749e549638fd4b734c999dab2cd400
Mantosh <kyamantoshkumar@gmail.com>
2023-01-06 10:40:35 +0530

    Update SaveData.js

--- src/Components/SaveData.js
+++ src/Components/SaveData.js
@@ -1,68 +1,68 @@
-const mongoose = require ("mongoose");
+// const mongoose = require ("mongoose");
 
-const userSchema = new mongoose.Schema({
-  id: {
-    type: String,
-    required: [true, "Please Enter Your Name"],
-  },
+// const userSchema = new mongoose.Schema({
+//   id: {
+//     type: String,
+//     required: [true, "Please Enter Your Name"],
+//   },
 
-  email: {
-    type: String,
-    unique: true,
-    required: [true, "Please Enter Your Email"],
-  },
+//   email: {
+//     type: String,
+//     unique: true,
+//     required: [true, "Please Enter Your Email"],
+//   },
 
-  password: {
-    type: String,
-    select: false,
-    required: [true, "Please Enter your password"],
-  },
+//   password: {
+//     type: String,
+//     select: false,
+//     required: [true, "Please Enter your password"],
+//   },
   
-  timeline: [
-    {
-      title: String,
-      description: String,
-      date: Date,
-    },
-  ],
-  skills: [
-    {
-      image: {
-        public_id: String,
-        url: String,
-      },
-      sname: String,
-      rating: String,
-    },
-  ],
-  address: {
-    address: String,
-    street: String,
-    pincode: String,
-  },
-  projects: [
-    {
-      url: String,
-      title: String,
-      image: {
-        public_id: String,
-        url: String,
-      },
-      description: String,
-      techStack: String,
-    },
-  ],
-  about: {
-    name: String,
-    title: String,
-    subtitle: String,
-    quote: String,
-    avtar: String,
-  },
-  profileIamge: {
-    public_id: String,
-    url: String,
-  },
-});
+//   timeline: [
+//     {
+//       title: String,
+//       description: String,
+//       date: Date,
+//     },
+//   ],
+//   skills: [
+//     {
+//       image: {
+//         public_id: String,
+//         url: String,
+//       },
+//       sname: String,
+//       rating: String,
+//     },
+//   ],
+//   address: {
+//     address: String,
+//     street: String,
+//     pincode: String,
+//   },
+//   projects: [
+//     {
+//       url: String,
+//       title: String,
+//       image: {
+//         public_id: String,
+//         url: String,
+//       },
+//       description: String,
+//       techStack: String,
+//     },
+//   ],
+//   about: {
+//     name: String,
+//     title: String,
+//     subtitle: String,
+//     quote: String,
+//     avtar: String,
+//   },
+//   profileIamge: {
+//     public_id: String,
+//     url: String,
+//   },
+// });
 
-export const User = mongoose.model("User", userSchema);
+// export const User = mongoose.model("User", userSchema);
